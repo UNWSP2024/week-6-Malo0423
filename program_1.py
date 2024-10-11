c78c2d464ae9c0b65a668239d933a09915e95b82
Malo0423 <mlorenson@students.unwsp.edu>
2024-10-11 15:51:31 -0500

    Add files via upload
    Author: Maggie Lorenson
    Program: Week 6 Program 1
    Date: 10/11/2024

--- program_1.py
+++ program_1.py
@@ -1,15 +1,22 @@
+import random
+TOSSES = 100
 
-# Program #1: Random Dice
-# Write a "randDice" function (with no input) that randomly chooses two numbers between 1 and 6 (inclusive) and then adds them (this is to simulate the rolling of 2 dice).  
-# The dice sum will be the output of this function.
 
 def randDice():
-    # Write your logic to generate 2 numbers between 1 and 6 here
+    dice1 = random.randint (1, 6)
+    dice2 = random.randint (1, 6)
+    dice_sum = dice1 + dice2
+    return dice_sum
+def main():
+    total = 0
+    for toss in range (TOSSES):
+        subtotal = randDice()
+        total += subtotal
+        average = total / 100
+    print(f'The average is {average:,.2f}')
 
-    # Sum 2 numbers
+if __name__ == '__main__':
+    main()
 
-    # return sum to calling function
 
-#########
-# Then write a mainline that calls the "randDice" function 100 times in a for loop.  
-# The mainline then prints the average of the 100 rolls, rounded to the nearest 0.01.
+
